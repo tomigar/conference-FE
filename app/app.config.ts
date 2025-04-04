b9626fe6f6151c85a70fe1a8c98fa6d08de3fdca
tomigar <tomas.gardlik@gmail.com>
2025-04-04 12:12:49 +0200

    initial

---
+++ app/app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  // https://ui.nuxt.com/getting-started/theme#design-system
+  ui: {
+    colors: {
+      primary: 'emerald',
+    },
+    button: {
+      defaultVariants: {
+        // Set default button color to neutral
+        color: 'primary'
+      }
+    }
+  }
+})
