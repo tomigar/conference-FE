ace9d807bd256a5be868e6501763f29c66e6028a
tomigar <tomas.gardlik@gmail.com>
2025-05-13 23:13:17 +0200

    add dashboard layout

--- app/layouts/Dashboard.vue
+++ app/layouts/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div class="flex">
+        <UNavigationMenu orientation="vertical" :items="items" class="data-[orientation=vertical]:w-48" />
         <header>
             <h1>Conference dashboard</h1>
         </header>
@@ -10,5 +11,33 @@
 </template>
 
 <script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
 
+const items = ref<NavigationMenuItem[][]>([
+  [
+    {
+      label: 'Links',
+      type: 'label'
+    },
+    {
+      label: 'Conferences',
+      icon: 'i-lucide-book-open',
+      to: '/dashboard',
+    //   children: [
+    //     {
+    //       label: 'Introducction',
+    //       description: 'Fully styled and customizable components for Nuxt.',
+    //       icon: 'i-lucide-house',
+    //     },
+    //   ]
+    },
+  ],
+  [
+    {
+      label: 'Editors',
+      icon: 'i-lucide-user-pen',
+      to: '/dashboard/editors',
+    },
+  ],
+])
 </script>
---
+++ app/pages/dashboard/editors.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        editors
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: 'dashboard',
+    title: 'Admin Dashboard',
+})
+</script>
+
+<style scoped>
+
+</style>
